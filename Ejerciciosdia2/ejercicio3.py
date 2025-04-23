fa425247ff0a9376419cc96ad07a6650e516ddef
BrahianMS <johnbra696@gmail.com>
2025-04-22 20:44:03 -0500

    Add files via upload

---
+++ Ejerciciosdia2/ejercicio3.py
@@ -0,0 +1,10 @@
+# 3. Lista vacía y append()
+
+#    Crea una lista vacía llamada nombres.
+#    Pide al usuario su nombre y agrégalo a la lista usando append().
+#    Muestra la lista final.
+
+nombre = []
+nom = str(input("ingrese su nombre: "))
+nombre.append(nom)
+print(nombre)
